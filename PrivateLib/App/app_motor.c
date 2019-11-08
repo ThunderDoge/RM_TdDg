cf9c0ebfe72fa5350c26cf3b962ec0c7acc28b15
ThunderDoge <thunderdoge@qq.com>
2019-11-08 21:19:57 +0800

    app_motor v0.2

--- PrivateLib/App/app_motor.c
+++ PrivateLib/App/app_motor.c
@@ -1,25 +1,42 @@
 /** 
-* @brief    个人临时电机库
-* @details  
-* @author   "ThunderDoge"玉熙成
-* @date      2019.11.1
-* @version  v0.1
-* @par Copyright (c):  OnePointFive, UESTC, 2019~2020
+ * @brief    个人临时电机库
+ * @details  
+ * @author   "ThunderDoge"玉熙成
+ * @date      2019.11.1
+ *  @version                                                                  *
+ *<Date>				|<Version>| <Author> 	| <Description>               *
+ *----------------------------------------------------------------------------*
+ *2019年11月8日 21:13	|v0.1	|ThunderDoge	|电机增加了软编码器类型 Motor_Soft.
+ * 
+ * @par Copyright (c):  OnePointFive, UESTC, 2019~2020
 	OnePointFIve, the UESTC RoboMaster Team.
-*/
+ */
 #include "app_motor.h"
 
 Motor_Data Motor_Yaw_Data; 
 Motor_Data Motor_Pitch_Data;
 
-
+#defien SIGN(VARIABLE) ((VARIABLE) > 0 ? 1 : 0)
 
 void app_Motor_Data_Analysis(Motor_Data* motor, uint8_t* Data)
 {
 	motor->StartUpdataTime = HAL_GetTick();
 	motor->LastPosition = motor->RealPosition;
 	motor->RealPosition = Data[0]<<8 | Data[1];
-	motor->RealSpeed    = Data[2]<<8 | Data[3];  
-  motor->RealCurrent  = Data[4]<<8 | Data[5];
+	motor->RealSpeed    = Data[2]<<8 | Data[3];
+  	motor->RealCurrent  = Data[4]<<8 | Data[5];
 	motor->RealTemperature = Data[6];
+}
+void app_Motor_SoftAnalysis(Motor_Soft* motor,uint8_t* Data)
+{
+	app_Motor_Data_Analysis(motor,Data);
+	int DeltaP = (motor->origin.RealPosition - motor->origin.LastPosition);
+	if( SIGN(motor->origin.RealSpeed) == SIGN(DeltaP) )
+	{
+		motor->soft.Position += DeltaP;
+	}
+	else //if( SIGN(motor->origin.RealSpeed) == SIGN(DeltaP) )
+	{
+		motor->soft.Position += (MOTOR6020_POSITION_PERIOD + DeltaP);
+	}
 }
--- PrivateLib/App/app_motor.h
+++ PrivateLib/App/app_motor.h
@@ -2,9 +2,11 @@
 #define	__APP_MOTOR_H_
 
 #include "stm32f4xx_hal.h"
+#include "app_private_math.h"
 
 #define MOTOR6020_POSITION_MIN	0
 #define MOTOR6020_POSITION_MAX	8191
+#define MOTOR6020_POSITION_PERIOD 8192
 
 #define MOTOR6020_CURRENT_MAX		30000
 
@@ -21,7 +23,17 @@ typedef struct
 	uint8_t RealTemperature;
 	uint32_t StartUpdataTime;
 }Motor_Data;
+typedef struct{
+	Motor_Data origin;
+	typedef struct 
+	{
+		Position;
+	}soft;
+}Motor_Soft;
 
 extern Motor_Data Motor_Yaw_Data, Motor_Pitch_Data;
+
+void app_Motor_Data_Analysis(Motor_Data* motor, uint8_t* Data);
+void app_Motor_SoftAnalysis(Motor_Soft* motor,uint8_t* Data);
 
 #endif	//__APP_MOTOR_H_
