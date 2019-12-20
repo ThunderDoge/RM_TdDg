382bff2fd01d29e72f315a23fbd21649f4b93dcb
ThunderDoge <thunderdoge@qq.com>
2019-12-20 17:09:10 +0800

    哨兵改写0X05 合并COMMU文件

--- Sentry_/SentiCloud0/MDK-ARM/Sentry/SentryCommu.hpp
+++ Sentry_/SentiCloud0/MDK-ARM/Sentry/SentryCommu.hpp
@@ -11,13 +11,26 @@
 #ifndef __SENTRY_COMMU_HPP_
 #define __SENTRY_COMMU_HPP_
 
+//外设定义
+
 //CAN板间通信定义，如果修CAN口请修改此处宏定义
 #define CAN_INTERBOARD hcan2
 
+//此板子使用了视觉串口
+#define USE_VISION
+
+//这是云台
+#define CLOUD_COMMU
+
+//这是底盘
+//#define CHASSIS_COMMU
+
+#ifdef USE_VISION
 #include "bsp_vision.hpp"
+#endif
+
 #include "bsp_can.hpp"
 #include "SentryCanCommu.hpp"
-#include "cmsis_os.h"
 enum SENTRY_CAN_ID : uint32_t //板间通讯ID号
 {
     UP_CLOUD_STATES = 0X101U,
@@ -35,6 +48,7 @@ enum SENTRY_CAN_ID : uint32_t //板间通讯ID号
     SUPERIOR_CHASSIS_SET_LOACTION_LIMIT_SPEED = 0X132U,
     SUPERIOR_SAFE = 0x1A0U,
 };
+#ifdef USE_VISION
 //视觉串口数据帧功能字
 enum __bsp_vision_Functionwords
 {
@@ -63,7 +77,7 @@ enum __bsp_vision_RobotError
     JUDG_OFFLINE = 0X07,
     REBOOTINT = 0X08,
 };
-
+#endif
 struct CanCommuRecv_t
 {
     uint32_t RecvId;
@@ -82,7 +96,7 @@ struct CanCommuRecv_t
     float Pillar_Dist;
     uint8_t Pillar_flag;
 };
-
+#ifdef USE_VISION
 //视觉传输数据解析结构体
 struct Sentry_vision_data
 {
@@ -113,12 +127,18 @@ struct Sentry_vision_data
     int16_t CAN1_motorlist = 0xffff; //CAN1电机列表
     int16_t CAN2_motorlist = 0xffff; //CAN2电机列表
 };
+#endif
+#ifdef CLOUD_COMMU
 enum _cloud_ctrl_mode:uint8_t
 {
     absolute_cloud = 0x01,
     relative_cloud = 0x02,
     save_cloud = 0x00,
 };
+#endif
+
+#ifndef __CHASSIS_MODE_DEF
+#define __CHASSIS_MODE_DEF
 enum _chassis_mode:uint8_t
 {
     _chassis_speed =1,
@@ -126,27 +146,40 @@ enum _chassis_mode:uint8_t
     _chassis_location_limit_speed =3,
     _chassis_save =0,
 };
+#endif	//__CHASSIS_MODE_DEF
 
 void CHASSIS_SUPERIOR_ALL_CanRx(uint32_t StdId, uint8_t *ptrData);
 
 
 //全局接收变量
 extern CanCommuRecv_t CanRx, CanTx;
+#ifdef USE_VISION
 extern Sentry_vision_data VisionRx,VisionTx;
-
+#endif
 // 主任务中周期性发送函数
+#ifdef CLOUD_COMMU
 void CloudCanCommuRoutine(void);
+#endif
+#ifdef CHASSIS_COMMU
+void ChassisCanCommuRoutine(void);
+#endif
+#ifdef USE_VISION
 void CloudVisonTxRoutine(void);
-
+#endif
 //CAN接收回调自动接收函数
+#ifdef CLOUD_COMMU
 void CanRxCpltCallBack_CloudCommuUpdata(CAN_HandleTypeDef *_hcan, CAN_RxHeaderTypeDef *RxHead, uint8_t *Data);
+#endif
 void CanRxCpltCallBack_ChassisCommuUpdata(CAN_HandleTypeDef *_hcan, CAN_RxHeaderTypeDef *RxHead, uint8_t *Data);
+//CAN信息底盘托管控制程序
+void ChassisCanRxHandle(void);
+#ifdef USE_VISION
 //视觉串口中断接收函数
 void SentryVisionUartRxAll(uint8_t* Vision_Rxbuffer);
 
 //视觉信息控制托管程序
 void VisionRxHandle(void);
-
+#endif
 
 //废案。
 //void UP_CLOUD_STATES_CanRx(int StdId, uint8_t *ptrData);
